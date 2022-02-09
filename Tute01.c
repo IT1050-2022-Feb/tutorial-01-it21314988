ea66101cb0452059526fe50a14de4c16552949ed
it21314988 <99327935+it21314988@users.noreply.github.com>
2022-02-09 16:29:51 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,14 @@
 #include <stdio.h>
 
 int main() {
+   float avg,n1,n2;
+   printf("enter the mark1-");
+   scanf("%f",&n1);
+   printf("enter the mark2-");
+   scanf("%f",&n2);
+   avg=(n1+n2)/2;
+   printf("average is-\n,&avg,&n1,n2");
+   
   
   return 0;
 }
